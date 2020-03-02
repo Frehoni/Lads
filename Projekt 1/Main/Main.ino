95ecfcd936dc4d8554c6b0effafc629d2b32a820
Frehoni <frehoni@gmail.com>
2020-03-02 12:43:45 +0100

    RTC og SD works

--- Projekt 1/Main/Main.ino
+++ Projekt 1/Main/Main.ino
@@ -17,47 +17,59 @@ File myFile;
 unsigned long delayTime;
 
 void setup() {
-  Serial.begin(9600);
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  Serial.print("Initializing SD card...");
-  delay(1000);
-  Serial.print("Pølse");
-  delay(1000);
-  //if (!SD.begin(4)) {
-    //Serial.println("initialization failed!");
-    //while (1);
-  //}
-  
-  
-
-  if (! rtc.isrunning()) {
-    Serial.println("RTC is NOT running!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-
-    // creates a new data file if one does not already exist
-    if (not SD.exists("DatA.txt")) {
-      myFile = SD.open("DatA.txt", FILE_WRITE);
-      myFile.print("DATE, TIME, TEMP *C, HUMIDITY");
-      myFile.println();
-      myFile.close();
-    }
-    // default settings
-    // (you can also pass in a Wire library object like &Wire2)
-    Serial.println(F("BME280 test"));
-    bool status;
-    status = bme.begin();
-    if (!status) {
-      Serial.println("Could not find a valid BME280 sensor, check wiring!");
-      while (1);
-    }
-    Serial.println("-- Default Test --");
-    delayTime = 1000;
-    Serial.println();
-
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
   }
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  if (not SD.exists("DatA.txt")) {
+    myFile = SD.open("DatA.txt", FILE_WRITE);
+    myFile.print("DATE, TIME, TEMP *C, HUMIDITY");
+    myFile.println();
+    myFile.close();
+  }
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.println("Writing to DatA.txt...");
+    myFile.close();
+  }
+  //Testing if RTC is working
+  DateTime now = rtc.now();
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.println(now.day(), DEC);
+  
+  // creates a new data file if one does not already exist
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  Serial.println(F("BME280 test"));
+  bool status;
+  status = bme.begin();
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+  Serial.println("-- Default Test --");
+  delayTime = 1000;
+  Serial.println();
+
 }
+
 
 
 void loop() {
