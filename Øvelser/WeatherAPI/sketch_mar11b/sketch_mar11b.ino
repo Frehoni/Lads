511e8a53d4f1a82d6fe68ba6b283dd62fb43f1cc
Frehoni <frehoni@gmail.com>
2020-03-28 10:56:13 +0100

    Update

--- Øvelser/WeatherAPI/sketch_mar11b/sketch_mar11b.ino
+++ Øvelser/WeatherAPI/sketch_mar11b/sketch_mar11b.ino
@@ -12,8 +12,8 @@
 #include <Wire.h>
 #endif
 
-const char* ssid = "OnePlus 6"; // HINT: create a hotspot with your phone..
-const char* password = "12345678";
+const char* ssid = "Powerpuff Pigerne"; // HINT: create a hotspot with your phone..
+const char* password = "BlomstogBobbel";
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
